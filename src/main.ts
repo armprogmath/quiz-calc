9b5caa1f0b9c68ef2c65e1ec62ddb1e5088b8e33
armprogmath <armprogmath@gmail.com>
2025-12-07 23:52:29 +0400

    integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Load the correct env file manually
+  dotenv.config({ path: '.development.env' });
+
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+
+  const port = configService.get('PORT') ?? 3300;
+
+  await app.listen(port);
+
+  Logger.log(
+    `${await app.getUrl()}, API Doc: ${configService.get('APP_URL')}`,
+  );
 }
-bootstrap();
+
+bootstrap().then(() => {
+  Logger.log(
+    `Quiz Calculation application successfully started in ${
+      process.env.NODE_ENV ?? 'development'
+    } mode`,
+  );
+});
