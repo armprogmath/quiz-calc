06c652204a2569eb0c6b540c97cd50ad76dece95
armprogmath <armprogmath@gmail.com>
2025-12-09 00:05:31 +0400

    Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:18-alpine
+
+WORKDIR /quiz_calc
+
+COPY package*.json ./
+
+# Install ALL dependencies (prod + dev) for Nest build
+RUN npm install
+
+# Copy entire project
+COPY . .
+
+# Build the NestJS project
+RUN npm run build
+
+# Remove dev dependencies (optional)
+RUN npm prune --production
+
+# Expose Nest port
+EXPOSE 3004
+
+CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
-import chalk from 'chalk';
+//import chalk from 'chalk';
+const chalk = require('chalk');
 
 async function bootstrap() {
   // Load the correct env file manually
@@ -25,4 +26,5 @@ async function bootstrap() {
   await app.listen(configService.get('PORT', 3003), async () => Logger.log(`${await app.getUrl()}, API Doc: ${configService.get('APP_URL')}`))
 }
 
-bootstrap().then(() => Logger.log(chalk.whiteBright.bgBlack` Quiz Calc ` + ` application successfully started in ${process.env.NODE_ENV || 'development' || 'production'} mode`));
+//bootstrap().then(() => Logger.log(chalk.whiteBright.bgBlack` Quiz Calc ` + ` application successfully started in ${process.env.NODE_ENV || 'development' || 'production'} mode`));
+bootstrap().then(() => Logger.log(`Quiz Calc application successfully started in ${process.env.NODE_ENV || 'development' || 'production'} mode`));
